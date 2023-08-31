02dd1b46652fcca9786a1680f756e174ed28376a
Miguel Martins <miguel.vale.martins@gmail.com>
2023-08-31 18:40:06 +0100

    Add function to play a single round

--- javascript.js
+++ javascript.js
@@ -11,3 +11,29 @@ function getComputerChoice() {
     return choice;
 }
 
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection.toUpperCase() === computerSelection.toUpperCase()){
+        return "It'a a Tie!";
+    }
+    switch(playerSelection.toUpperCase()){
+        case "ROCK":
+            if(computerSelection === "Paper"){
+                return "You Lose! Paper beats Rock"
+            }else{
+                return "You Win! Rock beats Scissors"
+            }
+        case "PAPER":
+            if(computerSelection === "Rock"){
+                return "You Win! Paper beats Rock"
+            }else{
+                return "You Lose! Scissors beats Paper"
+            }
+        case "SCISSORS":
+            if(computerSelection === "Paper"){
+                return "You Win! Scissors beats Paper"
+            }else{
+                return "You Lose! Rock beats Scissors"
+            }
+    }
+    return "Error! Player didn't choose neither \"Rock\" nor \"Paper\" nor \"Scissors\""
+}
