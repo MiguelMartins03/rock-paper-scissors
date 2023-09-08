eecbefb0fd208e18d8cbecb4ffc4075653690656
Miguel Martins <miguel.vale.martins@gmail.com>
2023-09-08 17:45:21 +0100

    Add buttons to facilitate playing the game

--- javascript.js
+++ javascript.js
@@ -13,52 +13,62 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if(playerSelection.toUpperCase() === computerSelection.toUpperCase()){
-        return "It'a a Tie!";
-    }
-    switch(playerSelection.toUpperCase()){
-        case "ROCK":
-            if(computerSelection === "Paper"){
-                return "You Lose! Paper beats Rock"
-            }else{
-                return "You Win! Rock beats Scissors"
-            }
-        case "PAPER":
-            if(computerSelection === "Rock"){
-                return "You Win! Paper beats Rock"
-            }else{
-                return "You Lose! Scissors beats Paper"
-            }
-        case "SCISSORS":
-            if(computerSelection === "Paper"){
-                return "You Win! Scissors beats Paper"
-            }else{
-                return "You Lose! Rock beats Scissors"
-            }
-    }
-    return "Error! Player didn't choose neither \"Rock\" nor \"Paper\" nor \"Scissors\""
-}
-
- 
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-    for(let i = 0; i < 5; i++){
-        let result = playRound(prompt("Choose either \"Rock\", \"Paper\" or \"Scissors\"", "Rock"), getComputerChoice());
-        let winOrLose = result.charAt(4);
-        if(winOrLose === "W"){
-            playerWins++;
-        }else if(winOrLose === "L"){
-            computerWins++;
-        }
-        console.log(result);
-    }
-    if(playerWins > computerWins){
-        return `You Win!\nTotal victories: ${playerWins}`;
-    }else if(playerWins < computerWins){
-        return `You Lose!\nTotal victories: ${playerWins}`;
+        console.log("It'a a Tie!");
     }else{
-        return `You Tied!\nTotal victories: ${playerWins}`;
+        switch(playerSelection.toUpperCase()){
+            case "ROCK":
+                if(computerSelection === "Paper"){
+                    console.log("You Lose! Paper beats Rock");
+                    break;
+                }else{
+                    console.log("You Win! Rock beats Scissors");
+                    break;
+                }
+            case "PAPER":
+                if(computerSelection === "Rock"){
+                    console.log("You Win! Paper beats Rock");
+                    break;
+                }else{
+                    console.log("You Lose! Scissors beats Paper");
+                    break;
+                }
+            case "SCISSORS":
+                if(computerSelection === "Paper"){
+                    console.log("You Win! Scissors beats Paper");
+                    break;
+                }else{
+                    console.log("You Lose! Rock beats Scissors");
+                    break;
+                }
+        }
     }
 }
 
-console.log(game());
+const buttons = document.querySelectorAll("button");
+buttons.forEach(btn => btn.addEventListener("click", (event) => {
+    playRound(event.target.id, getComputerChoice());
+}));
+ 
+// function game() {
+//     let playerWins = 0;
+//     let computerWins = 0;
+//     for(let i = 0; i < 5; i++){
+//         let result = playRound(prompt("Choose either \"Rock\", \"Paper\" or \"Scissors\"", "Rock"), getComputerChoice());
+//         let winOrLose = result.charAt(4);
+//         if(winOrLose === "W"){
+//             playerWins++;
+//         }else if(winOrLose === "L"){
+//             computerWins++;
+//         }
+//         console.log(result);
+//     }
+//     if(playerWins > computerWins){
+//         return `You Win!\nTotal victories: ${playerWins}`;
+//     }else if(playerWins < computerWins){
+//         return `You Lose!\nTotal victories: ${playerWins}`;
+//     }else{
+//         return `You Tied!\nTotal victories: ${playerWins}`;
+//     }
+// }
+
+// console.log(game());
